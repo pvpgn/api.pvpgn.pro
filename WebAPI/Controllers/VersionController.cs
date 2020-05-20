5f74b032aa068881aef211aaf10766e9c7779e08
HarpyWar <harpywar@gmail.com>
2020-05-20 17:47:28 +0300

    add /version endpoint

---
+++ WebAPI/Controllers/VersionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace WebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        // GET: /version
+        [HttpGet]
+        public Response Get()
+        {
+            return new SuccessResponse(new {
+                version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion,
+                runtime = PlatformServices.Default.Application.RuntimeFramework,
+                src = "https://github.com/pvpgn/api.pvpgn.pro"
+            });
+        }
+    }
+}
